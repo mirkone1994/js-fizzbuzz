f5c9b679c50ec7cfe3e83842adc9045cdc03fe71
mirkone1994 <mirkochiappori@gmail.com>
2021-07-05 15:20:38 +0200

    working script

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
 /*Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.*/
-var multiplesOf1 = []
-var multiplesOf3 = []
-var multiplesOf5 = []
-for (i=0; i<100; i++){
+/*var multiplesOf1 = [];
+var multiplesOf3 = [];
+var multiplesOf5 = [];*/
+var result = "";
+for (i=1; i<=100; i++){
+    if (i % 1 === 0) {
+        result = i;
+    }
     if (i % 3 === 0) {
-        multiplesOf3 = i
+        result = "fizz";
     }
-    else if (i % 5 === 0) {
-        multiplesOf5 = i
-    } else if (i % 1 === 0) {
-        multiplesOf1 = i
+    if (i % 5 === 0) {
+        result = "Buzz";
+    } 
+    if (i % 3 === 0 && i % 5 === 0) {
+        result = "FizzBuzz";
     }
-    console.log(multiplesOf3, multiplesOf5, multiplesOf1)
+    console.log(result)
 }
